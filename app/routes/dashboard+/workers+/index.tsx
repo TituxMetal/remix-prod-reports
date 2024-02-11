8fedddc651b02e0b286d09af038d3c5a7c865115
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-11 23:44:30 +0100

    Add WorkersIndexRoute component in dashboard

---
+++ app/routes/dashboard+/workers+/index.tsx
@@ -0,0 +1,12 @@
+import { PageLayout } from '~/components/common'
+
+const WorkersIndexRoute = () => {
+  return (
+    <PageLayout>
+      <h1 className='text-center text-2xl font-bold text-orange-300'>Dashboard Workers</h1>
+      <p>List of all workers</p>
+    </PageLayout>
+  )
+}
+
+export default WorkersIndexRoute
