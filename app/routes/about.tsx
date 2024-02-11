c515c16f8f15f91897e30ce273549f001ca31566
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-11 19:54:24 +0100

    Add About page

---
+++ app/routes/about.tsx
@@ -0,0 +1,15 @@
+import { PageLayout } from '~/components/common'
+
+const AboutRoute = () => {
+  return (
+    <PageLayout>
+      <h1 className='text-center text-3xl font-bold'>Welcome to LogiProdReport</h1>
+      <p>
+        Effortless downtime reporting for optimized logistics operations. Experience efficiency with
+        LogiProdReport.
+      </p>
+    </PageLayout>
+  )
+}
+
+export default AboutRoute
