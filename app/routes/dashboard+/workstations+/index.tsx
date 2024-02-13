0f2e473d7c4fc8a997f3736cad863e60e84f849d
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-13 11:01:44 +0100

    Add WorkstationsIndexRoute component in dashboard

---
+++ app/routes/dashboard+/workstations+/index.tsx
@@ -0,0 +1,12 @@
+import { PageLayout } from '~/components/common'
+
+const WorkstationsIndexRoute = () => {
+  return (
+    <PageLayout>
+      <h1 className='text-center text-2xl font-bold text-orange-300'>Dashboard Workstations</h1>
+      <p>List of all Workstations</p>
+    </PageLayout>
+  )
+}
+
+export default WorkstationsIndexRoute
