7165cd4f4dd9f9b09b2c0728a6ede879061a2009
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-14 11:17:59 +0100

    Refactor dashboard index page

--- app/routes/dashboard+/index.tsx
+++ app/routes/dashboard+/index.tsx
@@ -1,26 +1,15 @@
-import { Link } from '@remix-run/react'
-
-import { PageLayout } from '~/components/common'
-
 const DashboardIndexRoute = () => {
   return (
-    <PageLayout>
-      <h1 className='text-center text-2xl font-bold text-orange-300'>Dashboard</h1>
+    <div className='mt-6'>
       <p>Welcome to the dashboard.</p>
-      <div>
-        <ul className='flex gap-4 *:text-orange-300'>
-          <li>
-            <Link to='workers'>Show Workers</Link>
-          </li>
-          <li>
-            <Link to='reports'>Show Reports</Link>
-          </li>
-          <li>
-            <Link to='workstations'>Show Workstations</Link>
-          </li>
-        </ul>
+      <div className='flex flex-col gap-y-6'>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolores doloribus, adipisci
+          magnam sunt consequuntur non libero sint voluptatem maiores perspiciatis numquam explicabo
+          id eaque rem laudantium, similique doloremque optio velit.
+        </p>
       </div>
-    </PageLayout>
+    </div>
   )
 }
 
