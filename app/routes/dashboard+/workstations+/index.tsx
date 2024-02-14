0c45270dd8ac2db8823827286874634e4e6611bb
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-14 11:53:45 +0100

    Update dashboard worstations page layout

--- app/routes/dashboard+/workstations+/index.tsx
+++ app/routes/dashboard+/workstations+/index.tsx
@@ -1,11 +1,9 @@
-import { PageLayout } from '~/components/common'
-
 const WorkstationsIndexRoute = () => {
   return (
-    <PageLayout>
-      <h1 className='text-center text-2xl font-bold text-orange-300'>Dashboard Workstations</h1>
+    <div className='mt-6 px-12'>
+      <h1 className='mb-6 text-center text-2xl font-bold text-orange-300'>Workstations</h1>
       <p>List of all Workstations</p>
-    </PageLayout>
+    </div>
   )
 }
 
