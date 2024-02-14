6a8a5c4e5439f79753b86cf7637dea818eb774f8
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-14 11:52:29 +0100

    Update dashboard workers page layout

--- app/routes/dashboard+/workers+/index.tsx
+++ app/routes/dashboard+/workers+/index.tsx
@@ -1,11 +1,9 @@
-import { PageLayout } from '~/components/common'
-
 const WorkersIndexRoute = () => {
   return (
-    <PageLayout>
-      <h1 className='text-center text-2xl font-bold text-orange-300'>Dashboard Workers</h1>
+    <div className='mt-6 px-12'>
+      <h1 className='mb-6 text-center text-2xl font-bold text-orange-300'>Workers</h1>
       <p>List of all workers</p>
-    </PageLayout>
+    </div>
   )
 }
 
