66c47a39f8a40776f19c79bc8cdcbaae1bc8e84d
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-24 21:02:51 +0100

    fix: typo in create role admin page
    - change title to `Create Role`

--- app/routes/dashboard+/roles+/new.tsx
+++ app/routes/dashboard+/roles+/new.tsx
@@ -58,7 +58,7 @@ const NewRoleRoute = () => {
 
   return (
     <div className='mt-6 px-12'>
-      <h1 className='mb-6 text-center text-2xl font-bold text-orange-300'>Create User</h1>
+      <h1 className='mb-6 text-center text-2xl font-bold text-orange-300'>Create Role</h1>
       <Form
         method='post'
         {...getFormProps(form)}
