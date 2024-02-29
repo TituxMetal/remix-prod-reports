68141a35ad28784caf052a7d9f94cef8acefd070
Titux Metal (DEV) <github@lgdweb.fr>
2024-02-29 17:14:48 +0100

    feat(home): Home page and start button
    - Design the layout and UI components for the home page.
     - Implement the start button with appropriate style.
     - Clicking the start button should redirect users to Step 1 of the multi-step form.

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,5 +1,22 @@
-import { redirect } from '@remix-run/node'
+import { Link } from '@remix-run/react'
 
-export const loader = () => {
-  return redirect('/reports/start')
+const HomeRoute = () => {
+  return (
+    <div className='m-auto flex min-h-full flex-col items-center justify-center gap-y-2 bg-gray-900'>
+      <h1 className='px-4 text-center text-4xl font-bold leading-snug text-sky-600'>
+        Create New Reports
+      </h1>
+      <h2 className='px-4 text-center text-3xl font-bold leading-snug text-orange-600'>
+        In Only 4 Steps
+      </h2>
+      <Link
+        to='process/start'
+        className='mt-8 flex max-w-60 rounded-xl bg-sky-700 px-6 py-8 text-center text-3xl font-semibold uppercase text-orange-300 ring-4 ring-orange-600 hover:bg-sky-800 focus:bg-sky-800 focus-visible:outline-none'
+      >
+        Start Now
+      </Link>
+    </div>
+  )
 }
+
+export default HomeRoute
