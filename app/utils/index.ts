85b7ef861d33fee36affe89fddc30fbbfbee9031
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-03 21:41:09 +0100

    feat(auth): Add auth utility functions
    - validateUserInSession: Retrieves a user based on the provided key, session value, and role.
    - getUserIdByPersonalId: Retrieves the user ID based on the personal ID.
    - getAuthSessionInfo: Retrieves the authentication session information.
    - isStaffUser: Checks if the authenticated user is a staff user.

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,1 +1,8 @@
+export {
+  getAuthSessionInfo,
+  getUserIdByPersonalId,
+  isStaffUser,
+  validateUserInSession
+} from './auth.server'
+export { authSessionStorage } from './session.server'
 export { singleton } from './singleton.server'
