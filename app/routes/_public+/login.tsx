b175ca6734c934c2e4c78f6b7f2fb73bc35b6623
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-06 09:16:37 +0100

    feat(login): Implement login page
    - Create a new route/component for the login page.
    - Design and implement the UI layout for the login form with input fields and a submit button.
    - Implement client-side validation for input fields (e.g., required fields, valid personalId format).
    - Implement server-side logic to authenticate users and generate session cookie.
    - Handle successful login by redirecting users to the dashboard/userProfile route.
    - Display error messages for authentication failures or server errors.

--- app/routes/_public+/login.tsx
+++ app/routes/_public+/login.tsx
@@ -1,56 +1,145 @@
-import { redirect, type ActionFunctionArgs } from '@remix-run/node'
-import { Form } from '@remix-run/react'
+import { getFormProps, getInputProps, useForm } from '@conform-to/react'
+import { getZodConstraint, parseWithZod } from '@conform-to/zod'
+import {
+  redirect,
+  type ActionFunctionArgs,
+  type LoaderFunctionArgs,
+  type MetaFunction
+} from '@remix-run/node'
+import { Form, useActionData } from '@remix-run/react'
+import { z } from 'zod'
 
-import { PageLayout } from '~/components/common'
+import { WORKER_ROLE } from '~/constants'
+import { authSessionStorage } from '~/utils'
+import {
+  getAuthSessionInfo,
+  isStaffUser,
+  validateUserInSession,
+  verifyUser
+} from '~/utils/auth.server'
+
+const loginSchema = z.object({
+  identifier: z
+    .string({ required_error: 'Personal ID or Username is required' })
+    .min(5, { message: 'Identifier must be at least 5 characters long' })
+    .max(8, { message: 'Identifier must be at most 8 characters long' }),
+  password: z.string({ required_error: 'Password is required' })
+})
 
 export const action = async ({ request }: ActionFunctionArgs) => {
   const formData = await request.formData()
-  const { personalId, password } = Object.fromEntries(formData)
+  const submission = parseWithZod(formData, { schema: loginSchema })
 
-  if (String(personalId).trim() === '' || String(password).trim() === '') {
-    return new Response('Bad Request', { status: 400 })
+  if (submission.status !== 'success') {
+    return submission.reply({ hideFields: ['password'] })
   }
 
-  // TODO: Implement login logic
+  const { identifier, password } = submission.value
+  const user = await verifyUser(identifier, password)
 
-  return redirect('/dashboard')
+  if (!user) {
+    return submission.reply({ hideFields: ['password'], formErrors: ['Invalid credentials'] })
+  }
+
+  const authSession = await authSessionStorage.getSession(request.headers.get('Cookie'))
+  const { id, personalId, role } = user
+
+  authSession.set('userId', id)
+  authSession.set('personalId', personalId)
+  authSession.set('role', role.name)
+
+  const commitSession = await authSessionStorage.commitSession(authSession)
+
+  if (role.name === WORKER_ROLE) {
+    throw redirect('/profile', { headers: { 'Set-Cookie': commitSession } })
+  }
+
+  throw redirect('/dashboard', { headers: { 'Set-Cookie': commitSession } })
+}
+
+export const loader = async ({ request }: LoaderFunctionArgs) => {
+  const authSession = await authSessionStorage.getSession(request.headers.get('Cookie'))
+  const { sessionRole, sessionUserId } = getAuthSessionInfo(authSession)
+  const authenticatedUser = await validateUserInSession({
+    key: 'id',
+    sessionValue: sessionUserId,
+    role: sessionRole
+  })
+  const staffUser = await isStaffUser(authSession)
+
+  if (authenticatedUser && staffUser) {
+    return redirect('/dashboard')
+  }
+
+  if (authenticatedUser && sessionRole === WORKER_ROLE) {
+    return redirect('/profile')
+  }
+
+  return {}
+}
+
+export const meta: MetaFunction = () => {
+  return [{ title: 'LogiProdReport | Log In' }]
 }
 
 const LoginRoute = () => {
+  const lastResult = useActionData<typeof action>()
+  const [form, fields] = useForm({
+    lastResult,
+    constraint: getZodConstraint(loginSchema),
+    shouldValidate: 'onBlur',
+    shouldRevalidate: 'onInput',
+    onValidate: ({ formData }) => parseWithZod(formData, { schema: loginSchema })
+  })
+
   return (
-    <PageLayout>
-      <h1 className='text-center text-2xl font-bold text-orange-300'>Log In to dashboard</h1>
-      <Form method='post' className='flex flex-col gap-4'>
+    <div className='m-auto flex min-h-full flex-col items-center gap-y-2 bg-gray-900 sm:justify-center'>
+      <h1 className='px-4 text-center text-4xl font-bold leading-snug text-sky-600'>Log In to</h1>
+      <h2 className='px-4 text-center text-2xl font-semibold leading-snug text-orange-600'>
+        Logi Prod Report
+      </h2>
+      <Form method='post' {...getFormProps(form)} className='flex flex-col gap-4'>
+        {form.errors && (
+          <p className='text-xs text-red-300' id={form.errorId}>
+            {form.errors}
+          </p>
+        )}
         <div className='flex flex-col gap-2'>
-          <label htmlFor='personalId-field'>Personal ID</label>
+          <label htmlFor={fields.identifier.id}>Identifier</label>
           <input
-            className='rounded-md border border-orange-300 bg-gray-700 text-gray-200 focus:border-orange-300 focus:outline-none focus:ring-1 focus:ring-orange-600 focus:ring-offset-gray-900'
-            type='text'
-            name='personalId'
-            id='personalId-field'
-            placeholder='Personal ID'
+            className='rounded-md border-2 border-orange-400 bg-gray-800 text-gray-200 placeholder:text-gray-400 focus:border-orange-600 focus:outline-none focus:ring-1 focus:ring-orange-600 focus:ring-offset-gray-900'
+            {...getInputProps(fields.identifier, { type: 'text' })}
+            placeholder='Personal ID or Username'
           />
+          {fields.identifier.errors && (
+            <p className='text-xs text-red-300' id={fields.identifier.errorId}>
+              {fields.identifier.errors}
+            </p>
+          )}
         </div>
         <div className='flex flex-col gap-2'>
-          <label htmlFor='password-field'>Password</label>
+          <label htmlFor={fields.password.id}>Password</label>
           <input
-            className='rounded-md border border-orange-300 bg-gray-700 text-gray-200 focus:border-orange-300 focus:outline-none focus:ring-1 focus:ring-orange-600 focus:ring-offset-gray-900'
-            type='password'
-            name='password'
-            id='password-field'
+            className='rounded-md border-2 border-orange-400 bg-gray-800 text-gray-200 placeholder:text-gray-400 focus:border-orange-600 focus:outline-none focus:ring-1 focus:ring-orange-600 focus:ring-offset-gray-900'
+            {...getInputProps(fields.password, { type: 'password' })}
             placeholder='Password'
           />
+          {fields.password.errors && (
+            <p className='text-xs text-red-300' id={fields.password.errorId}>
+              {fields.password.errors}
+            </p>
+          )}
         </div>
         <div>
           <button
             type='submit'
-            className='rounded-md border border-orange-300 bg-transparent px-2 py-3 text-orange-300 hover:bg-orange-400 hover:text-gray-900'
+            className='rounded-md bg-sky-700 px-4 py-2 text-lg font-semibold text-orange-300 ring-2 ring-orange-500 hover:bg-sky-800 focus:bg-sky-800 focus-visible:outline-none'
           >
             Log In
           </button>
         </div>
       </Form>
-    </PageLayout>
+    </div>
   )
 }
 
