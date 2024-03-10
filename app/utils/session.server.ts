916c2d58fa43c53b4013918d78faba744d861c83
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-10 02:22:02 +0100

    feat(session): Add getCookieSession and getSessionExpirationDate functions
    - getCookieSession: Retrieves the authentication session from the provided request's cookie.
    - getSessionExpirationDate: Returns the expiration date of the session.
    - add docs to authSessionStorage.
    - remove AUTH_SESSION_SECRET, use constants instead.

--- app/utils/session.server.ts
+++ app/utils/session.server.ts
@@ -1,7 +1,15 @@
 import { createCookieSessionStorage } from '@remix-run/node'
 
-const AUTH_SESSION_SECRET = process.env.AUTH_SESSION_SECRET || ''
+import {
+  AUTH_SESSION_SECRET,
+  SESSION_EXPIRATION_TIME,
+  SHORT_SESSION_EXPIRATION_TIME
+} from '~/constants'
 
+/**
+ * Creates a cookie session storage for authentication.
+ * @returns The created cookie session storage.
+ */
 export const authSessionStorage = createCookieSessionStorage({
   cookie: {
     name: 'lpr_auth_session',
@@ -12,3 +20,25 @@ export const authSessionStorage = createCookieSessionStorage({
     secure: process.env.NODE_ENV === 'production'
   }
 })
+
+/**
+ * Retrieves the authentication session from the provided request's cookie.
+ * @param request - The request object containing the headers.
+ * @returns A promise that resolves to the authentication session.
+ */
+export const getCookieSession = async (request: Request) => {
+  const authSession = await authSessionStorage.getSession(request.headers.get('Cookie'))
+
+  return authSession
+}
+
+/**
+ * Returns the expiration date of the session.
+ * @param short - Optional parameter to specify whether to use the short session expiration time.
+ * @returns The expiration date of the session.
+ */
+export const getSessionExpirationDate = (short?: boolean) => {
+  const expirationTime = short ? SHORT_SESSION_EXPIRATION_TIME : SESSION_EXPIRATION_TIME
+
+  return new Date(Date.now() + expirationTime)
+}
