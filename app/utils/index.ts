46317a12a8662f1c381ecef4b14586855349dc10
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-10 02:31:43 +0100

    feat(user): Add user utility functions
    - useOptionalUser: Returns the optional user data from the route loader.
    - useUser: Custom hook that returns the user object.
    - update exports in index

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -1,8 +1,14 @@
 export {
   getAuthSessionInfo,
+  getUserByIdentifier,
   getUserIdByPersonalId,
   isStaffUser,
+  login,
+  logout,
+  requireAdminUser,
+  requireStaffUser,
   validateUserInSession
 } from './auth.server'
-export { authSessionStorage } from './session.server'
+export { authSessionStorage, getCookieSession, getSessionExpirationDate } from './session.server'
 export { singleton } from './singleton.server'
+export { useOptionalUser, useUser } from './user'
