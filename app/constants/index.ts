57478fbbb939f69dc87900e6f9f4cd183441aab9
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-10 02:10:03 +0100

    refactor(constants): Move worker role, add staff roles and session related constants

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,7 +1,7 @@
+export { STEP_FOUR_INTENT, STEP_ONE_INTENT, STEP_THREE_INTENT, STEP_TWO_INTENT } from './process'
+export { StaffRoles, WORKER_ROLE } from './roles'
 export {
-  STEP_FOUR_INTENT,
-  STEP_ONE_INTENT,
-  STEP_THREE_INTENT,
-  STEP_TWO_INTENT,
-  WORKER_ROLE
-} from './process'
+  AUTH_SESSION_SECRET,
+  SESSION_EXPIRATION_TIME,
+  SHORT_SESSION_EXPIRATION_TIME
+} from './sessions'
