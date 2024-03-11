352dda5eb78cd16e99c6f8436afdea24eaf4dd2a
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-12 00:30:22 +0100

    feat(dashboard): update dashboard navigation menu
    - Update the link to be shown for admin and non admin users

--- app/routes/dashboard+/_layout.tsx
+++ app/routes/dashboard+/_layout.tsx
@@ -31,17 +31,25 @@ const DashboardLayout = () => {
       <main className='-mt-16'>
         <div className='mx-auto max-w-screen-2xl px-4 pb-12 sm:px-6 lg:px-8'>
           <div className='rounded-lg bg-gray-800 px-5 pb-6 sm:px-6'>
-            <ul className='flex w-full justify-center gap-2'>
-              {isAdminUser ? (
+            <ul className='flex justify-center gap-2'>
+              <li className='mx-1'>
+                <Link
+                  className='inline-block rounded-b-md border-x-2 border-b-2 border-sky-700 bg-orange-800 px-3 py-1.5 font-semibold text-orange-200 hover:bg-orange-900 hover:text-orange-100'
+                  to='/dashboard'
+                >
+                  Home
+                </Link>
+              </li>
+              <li className='mx-1'>
+                <Link
+                  className='inline-block rounded-b-md border-x-2 border-b-2 border-sky-700 bg-orange-800 px-3 py-1.5 font-semibold text-orange-200 hover:bg-orange-900 hover:text-orange-100'
+                  to='workers'
+                >
+                  Workers
+                </Link>
+              </li>
+              {isAdminUser && (
                 <>
-                  <li className='mx-1'>
-                    <Link
-                      className='inline-block rounded-b-md border-x-2 border-b-2 border-sky-700 bg-orange-800 px-3 py-1.5 font-semibold text-orange-200 hover:bg-orange-900 hover:text-orange-100'
-                      to='/dashboard'
-                    >
-                      Home
-                    </Link>
-                  </li>
                   <li className='mx-1'>
                     <Link
                       className='inline-block rounded-b-md border-x-2 border-b-2 border-sky-700 bg-orange-800 px-3 py-1.5 font-semibold text-orange-200 hover:bg-orange-900 hover:text-orange-100'
@@ -83,15 +91,6 @@ const DashboardLayout = () => {
                     </Link>
                   </li>
                 </>
-              ) : (
-                <li className='mx-1'>
-                  <Link
-                    className='inline-block rounded-b-md border-x-2 border-b-2 border-sky-700 bg-orange-800 px-3 py-1.5 font-semibold text-orange-200 hover:bg-orange-900 hover:text-orange-100'
-                    to='workers'
-                  >
-                    Workers
-                  </Link>
-                </li>
               )}
             </ul>
             <Outlet />
