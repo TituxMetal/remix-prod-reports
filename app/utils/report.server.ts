288d0f6a36115043af3d4b1901b69ba158313825
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-14 04:26:38 +0100

    feat(utils): Adds report utility functions
    - groupReportsByDay: Groups an array of reports by day.
    - sortReportsByDay: Sorts the reports by day and formats the start and end times.

---
+++ app/utils/report.server.ts
@@ -0,0 +1,68 @@
+import { format } from 'date-fns'
+
+interface Report {
+  id: string
+  startDate: Date
+  endDate: Date
+  reasonForDowntime: string
+  storageLocation: string | null
+  details: string | null
+  duration: number
+  statusName: string
+  workstation: {
+    name: string
+    displayName: string
+  }
+}
+
+interface GroupedReports {
+  [date: string]: Report[]
+}
+
+interface SortedReport extends Report {
+  startTime: string
+  endTime: string
+}
+
+interface SortedReportsByDay {
+  dateOfDay: string
+  reports: SortedReport[]
+}
+
+/**
+ * Groups an array of reports by day.
+ * @param reports - The array of reports to be grouped.
+ * @returns An object where the keys are the dates in 'yyyy-MM-dd' format and the values are arrays of reports for each date.
+ */
+export const groupReportsByDay = (reports: Report[]): GroupedReports => {
+  const reportsByDay: GroupedReports = {}
+  reports.forEach(report => {
+    const dateOfDay = format(report.startDate, 'yyyy-MM-dd')
+    if (!reportsByDay[dateOfDay]) {
+      reportsByDay[dateOfDay] = []
+    }
+    reportsByDay[dateOfDay].push(report)
+  })
+  return reportsByDay
+}
+
+/**
+ * Sorts the reports by day and formats the start and end times.
+ *
+ * @param reportsByDay - The grouped reports by day.
+ * @returns An array of sorted reports by day with formatted start and end times.
+ */
+export const sortReportsByDay = (reportsByDay: GroupedReports): SortedReportsByDay[] => {
+  const sortedDays = Object.keys(reportsByDay).sort()
+  const sortedReportsByDay = sortedDays.map(dateOfDay => ({
+    dateOfDay,
+    reports: reportsByDay[dateOfDay]
+      .map(report => ({
+        ...report,
+        startTime: format(new Date(report.startDate), 'HH:mm'),
+        endTime: format(new Date(report.endDate), 'HH:mm')
+      }))
+      .sort((a, b) => a.startDate.getTime() - b.startDate.getTime())
+  }))
+  return sortedReportsByDay
+}
