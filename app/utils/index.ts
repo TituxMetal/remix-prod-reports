e0cf3b335046aa2c9ffd9f9f01519d83c9e7fb55
Titux Metal (DEV) <github@lgdweb.fr>
2024-03-14 04:26:38 +0100

    feat(utils): Adds report utility functions
    - groupReportsByDay: Groups an array of reports by day.
    - sortReportsByDay: Sorts the reports by day and formats the start and end times.

--- app/utils/index.ts
+++ app/utils/index.ts
@@ -9,6 +9,7 @@ export {
   requireStaffUser,
   validateUserInSession
 } from './auth.server'
+export { groupReportsByDay, sortReportsByDay } from './report.server'
 export { authSessionStorage, getCookieSession, getSessionExpirationDate } from './session.server'
 export { singleton } from './singleton.server'
 export { useOptionalUser, useUser } from './user'
