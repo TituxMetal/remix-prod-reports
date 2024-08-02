67a37dd575d678a3ec162f9b514ef5f27e88a969
Titux Metal (DEV) <github@lgdweb.fr>
2024-08-02 05:58:59 +0200

    chore: Move the AUTH_SESSION_SECRET constant.
    This is causing server bundling issues due to the process.env that is server specific.
    Move the AUTH_SESSION_SECRET constant from the constants/sessions.ts file to the utils/session.server.ts resolves the problem.

--- app/constants/index.ts
+++ app/constants/index.ts
@@ -1,7 +1,3 @@
 export { STEP_FOUR_INTENT, STEP_ONE_INTENT, STEP_THREE_INTENT, STEP_TWO_INTENT } from './process'
 export { StaffRoles, WORKER_ROLE } from './roles'
-export {
-  AUTH_SESSION_SECRET,
-  SESSION_EXPIRATION_TIME,
-  SHORT_SESSION_EXPIRATION_TIME
-} from './sessions'
+export { SESSION_EXPIRATION_TIME, SHORT_SESSION_EXPIRATION_TIME } from './sessions'
